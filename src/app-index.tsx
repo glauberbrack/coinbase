bf140c61a2f63360a076699b9ba0d448555cadd2
glauberbrack <glauber@brack.com.br>
2024-07-04 11:18:04 -0300

    feat(home): create scene

---
+++ src/app-index.tsx
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Home} from './scenes';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  );
+};
+
+export default App;
