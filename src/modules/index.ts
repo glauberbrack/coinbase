84ba8568d3a5410df498f2279877bff9c585fd52
glauberbrack <glauber@brack.com.br>
2024-07-04 13:29:47 -0300

    refactor(modules): move imports to correct folder

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,2 +1,5 @@
 export {default as axios, AxiosError} from 'axios';
 export type {AxiosRequestConfig, AxiosResponse} from 'axios';
+
+export {configureStore, createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+export type {PayloadAction} from '@reduxjs/toolkit';
